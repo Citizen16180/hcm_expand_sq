4b6bbe1f1fa91fab52914c91e6df93f3da2b9d14
Citizen16180 <146931316+Citizen16180@users.noreply.github.com>
2023-10-06 17:32:09 +0300

    Update expand.js

--- expand.js
+++ expand.js
@@ -1,1 +1,15 @@
+window.onload = function() {
+    setTimeout(expand, 5000);
+};
 
+function expand() {
+    var elements = document.querySelectorAll('.x3ae.x3ad.hideInEditMode.xeq.p_AFIconOnly > a');
+
+    elements.forEach(function(element) {
+        if(element) {
+            element.click();
+        } else {
+            console.log('Element not found!');
+        }
+    });
+}
